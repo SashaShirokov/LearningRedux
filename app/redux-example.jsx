fdf63eaeadb6a6286cb0b101e540f28f927d19e2
Sasha <lyshirokov613@gmail.com>
2016-12-20 17:27:54 +0300

    Add reducer and start redux

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,39 +2,12 @@ var redux = require("redux");
 
 console.log("Starting redux example!!!!!!!!!!!!");
 
-// Pure function
-function add(a, b) {
-	return a + b;
-}
+var reducer = (state = {name: "Anonymous"}, action) => {
+	// state = state || {name: "Anonymous"};
 
-var a =3;
-function add(b) {
-	return a + b;
-}
+	return state;
+};
+var store = redux.createStore(reducer);
 
-var result;
-function add(a, b) {
-	result = a + b;
-	return result;
-}
-
-function add(a, b) {
-	return a + b + new Date().getSeconds();
-}
-
-function changeProp(obj) {
-	obj.name = "Jen";
-	return obj;
-	// return {
-	// 	...obj,
-	// 	name: "Jen"
-	// }
-}
-
-var startingValue = {
-	name: "Sasha",
-	age: 38
-}
-var res = changeProp(startingValue);
-console.log(startingValue);
-console.log(res);
+var currentState = store.getState();
+console.log("currentState", currentState);
